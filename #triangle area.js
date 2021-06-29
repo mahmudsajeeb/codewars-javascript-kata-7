5861e9809e9244c110837f645dee7f1c99ff3475
mahmudsajeeb <67100028+mahmudsajeeb@users.noreply.github.com>
2021-06-30 01:27:28 +0600

    Add files via upload

---
+++ #triangle area.js
@@ -0,0 +1,27 @@
+
+/**
+ * 
+ Task.
+
+Calculate area of given triangle. Create a function t_area that will take a string which will represent triangle, find area of the triangle, one space will be equal to one length unit. The smallest triangle will have one length unit.
+
+Hints
+
+Ignore dots.
+
+Example:
+
+.
+.      .  
+.      .       .      ---> should return 2.0
+.
+.      .  
+.      .       .     
+.      .       .      .      ---> should return 4.5
+ */
+
+
+function tArea(tStr) {
+  const side = tStr.split('\n').length-3;
+  return (side * side)/2
+}
